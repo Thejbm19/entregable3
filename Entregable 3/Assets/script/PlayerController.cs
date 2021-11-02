ac573ec289fe27b08e9e885c52d1f03f578b2005
Thejbm19 <jaumebauza10@gmail.com>
2021-11-02 08:24:37 +0100

    treball

---
+++ Entregable 3/Assets/script/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.localScale += 2*Vector3.one;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.rotation *= Quaternion.Euler(45, 0, 45);
+        }
+
+    }
+
+
+
+
+
+
+
+}
